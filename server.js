7132d245311b083b3f55f2328e1abf9ff363fd04
Abhishek Rajput <rabhi0766@gmail.com>
2025-02-17 13:01:29 +0530

    socket connection

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ const server = app.listen(PORT, () => {
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://chat-app-frontend-rosy-mu.vercel.app/",
     credentials: true,
   },
 });
