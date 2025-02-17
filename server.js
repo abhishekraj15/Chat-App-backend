f7f4091dea87c950917b4bc1c893f6a65b594b0f
Abhishek Rajput <rabhi0766@gmail.com>
2025-02-17 12:00:13 +0530

    cors

--- server.js
+++ server.js
@@ -12,7 +12,13 @@ connectDB();
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+// app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("API is running..");
