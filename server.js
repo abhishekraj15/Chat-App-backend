a5c282926336278a8eeee211eac84d66e4051bb5
Abhishek Rajput <rabhi0766@gmail.com>
2025-02-17 13:00:33 +0530

    cors added

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 // app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(
   cors({
-    origin: "*",
+    origin: "https://chat-app-frontend-rosy-mu.vercel.app/",
     credentials: true,
   })
 );
